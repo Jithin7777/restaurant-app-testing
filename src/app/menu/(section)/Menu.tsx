c6e0a998e5e9c2a67e4601037da7a5bed2264187
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-05-28 20:42:36 +0530

    Fix: update Menu and CardList components

--- src/app/menu/(section)/Menu.tsx
+++ src/app/menu/(section)/Menu.tsx
@@ -35,7 +35,7 @@ const menuItems = [
 
 const PopularMenu = () => {
   return (
-    <section id='popular' className="mt-10 flex flex-col gap-10 ml-0  xl:ml-10">
+    <section id='popular' className="mt-10 flex flex-col  gap-10 ml-0 pb-28  xl:ml-10">
       <div className="flex items-center justify-between gap-3 px-4 md:px-10 xl:px-0">
         <h2 className="text-primary text-xl font-semibold md:text-2xl lg:text-3xl">
           Popular
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,48 +6,37 @@ import Menu from "./(section)/Menu";
 
 const Page = () => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col lg:px-16">
       {/* Navbar */}
-    <div className="fixed top-0 z-50 h-16 w-full bg-gray-50 md:bg-transparent">
+      <div className="fixed top-0 z-50 h-16 w-full bg-gray-50 md:bg-transparent">
         <Navbar />
       </div>
 
       {/* Main Content Area */}
       <div className="hide-scrollbar mt-48 flex flex-1 flex-col md:mt-20 lg:flex-row">
-        
         {/* Mobile Sidebar (search fixed, categories scrollable) */}
-        <div className="fixed top-0 pt-24 z-40 w-full bg-gray-50 px-4 py-2 md:hidden">
+        <div className="fixed top-0 z-40 w-full bg-gray-50 px-4 py-2 pt-24 md:hidden">
           <SidebarNav />
         </div>
 
         {/* Responsive Desktop Sidebar */}
-{/* Responsive Desktop Sidebar */}
-<div
-  className={`
-    hide-scrollbar hidden md:block
-    w-full overflow-x-auto 
-    lg:w-72 lg:h-[calc(100dvh-4rem)] lg:border-b-0  lg:overflow-y-auto lg:fixed lg:top-20
-  `}
->
-  <div className="flex md:flex-row lg:flex-col p-4 w-max lg:w-auto">
-    <SidebarNav />
-  </div>
-</div>
+        {/* Responsive Desktop Sidebar */}
+        <div
+          className={`hide-scrollbar hidden w-full overflow-x-auto md:block lg:fixed lg:top-20 lg:h-[calc(100dvh-4rem)] lg:w-72 lg:overflow-y-auto lg:border-b-0`}
+        >
+          <div className="flex w-max p-4 md:flex-row lg:w-auto lg:flex-col">
+            <SidebarNav />
+          </div>
+        </div>
 
         {/* Main Content */}
-        <div className={`
-          hide-scrollbar mt-8 flex-1 overflow-x-hidden
-          md:mt-72
-          
-          lg:ml-72  lg:mt-0
-        `}>
-          <div className="mx-auto max-w-5xl  py-6 md:pt-8">
+        <div
+          className={`hide-scrollbar mt-8 flex-1 overflow-x-hidden md:mt-16 lg:mt-0 lg:ml-72`}
+        >
+          <div className="mx-auto max-w-5xl py-6 md:pt-8">
             <PickupDeliveryInfo />
             <Menu />
-                       
-
           </div>
-          
         </div>
       </div>
     </div>

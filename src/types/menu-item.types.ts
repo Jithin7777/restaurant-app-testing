6e1860d6f613aec471cfb7eaef9557d1e79119c4
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-05-31 11:57:50 +0530

    Deleted .gitattributes and updated globals.css and menu-item.types.ts

--- src/types/menu-item.types.ts
+++ src/types/menu-item.types.ts
@@ -1,8 +1,8 @@
 export interface MenuItem {
-  id: string | number;        // Accept both if needed, but better to pick one
+  id: string | number;        
   title: string;
-  price: string      // Use number ideally, but if you store formatted price, string is fine
-  description?: string;       // Optional if not always used
+  price: string      
+  description?: string;     
   likes?: number;
   image: string;
 }

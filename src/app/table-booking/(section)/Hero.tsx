a820d699a26e4119eed331c51f2f1f8c7429d365
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-06-22 23:00:52 +0530

    update hero section

--- src/app/table-booking/(section)/Hero.tsx
+++ src/app/table-booking/(section)/Hero.tsx
@@ -11,7 +11,7 @@ const Hero:React.FC<HeroProps> = ({onBookNowClick}) => {
 
 
   return (
-    <section className="flex min-h-[500px] w-full flex-col items-center justify-center gap-6 bg-[url('/images/table-booking/hero.jpg')] bg-cover bg-[center_bottom] bg-no-repeat text-[1.5rem] leading-[110%]">
+    <section className="flex min-h-[500px] w-full flex-col items-center justify-center gap-6 bg-[url('/images/table-booking/Hero.jpg')] bg-cover bg-[center_bottom] bg-no-repeat text-[1.5rem] leading-[110%]">
       <button  onClick={onBookNowClick} className="bg-[#33bb60] text-white text-[18px] py-3 px-6 flex items-center gap-3 rounded">
         Book Now
         <CheckIcon className="w-6 h-6 text-white" />
